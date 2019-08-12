e4b47a0f0cf883696c383921caf013f99aa0e7ab
Mujtaba Badar <mujbadar@macbook-pro.lan>
2019-08-12 14:21:46 -0500

    ignore this

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,7 @@
 class Account < ApplicationRecord
+  has_secure_password
+
+  validates :user_name, presence: true, unique:true
   has_many :projects
   has_many :bids
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :installs
   root 'accounts#index'
   resources :bids
   resources :accounts do
@@ -7,5 +8,6 @@ Rails.application.routes.draw do
     end
   end
   resources :projects, only: [:index, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
